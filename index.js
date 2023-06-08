440f7830ad61803003ced0e157231d63df17cfe6
Astro <rgrxmahabub.official@gmail.com>
2023-06-08 21:03:03 +0600

    allclass and allinstructors created

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-const express = require('express')
-const app = express()
-const jwt = require('jsonwebtoken')
-const morgan = require('morgan')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200,
-  }
-  app.use(cors(corsOptions))
-  app.use(express.json())
-  app.use(morgan('dev'))
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+// app.use(morgan("dev"));
 
-
-
-
-  
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ig2b4pm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,29 +25,49 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const classesCollection = client.db("goldenPeaks").collection("allClass");
+    const instructorCollection = client.db("goldenPeaks").collection("instructors");
+
+    app.get("/class", async (req, res) => {
+      const result = await classesCollection.find().sort({ number_of_students: -1 }).toArray();
+      res.send(result);
+    });
+    app.get("/instructors", async (req, res) => {
+      const result = await instructorCollection.find().sort({ number_of_students: -1 }).toArray();
+      res.send(result);
+    });
+
+    
+    
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Golden Peaks Server is running..");
+});
 
-  app.get('/', (req, res) => {
-    res.send('Golden Peaks Server is running..')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Golden Peaks is running on port ${port}`)
-  })
-  
+app.listen(port, () => {
+  console.log(`Golden Peaks is running on port ${port}`);
+});
